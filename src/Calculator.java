5493b272f46dbac6b4c3a0bf56b84489383e75e2
kapellan <nortpacificeagle@gmail.com>
2017-04-22 16:14:36 +0300

    Multiply feature has been added

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,10 @@
  */
 public class Calculator {
     public double add(double num1, double num2) {
-        return num1+num2;
+        return num1 + num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
     }
 }
