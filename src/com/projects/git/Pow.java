48fd329cb5d1191316dbc8378a996ad1fdd03630
Frost703 <frost703@ukr.net>
2017-04-22 16:57:02 +0300

    banking calculator added new to2 and to3 features

---
+++ src/com/projects/git/Pow.java
@@ -0,0 +1,22 @@
+package com.projects.git;
+
+/**
+ * Created by O'Neill on 4/22/2017.
+ */
+public class Pow {
+    public int to2(int num){
+        return num*num;
+    }
+
+    public int to3(int num){
+        return num*num*num;
+    }
+
+    public double to2(double num) {
+        return num*num;
+    }
+
+    public double to3(double num){
+        return num*num*num;
+    }
+}
